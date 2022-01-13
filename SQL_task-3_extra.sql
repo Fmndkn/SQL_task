7ba96198f0f79b14571834b062ce7b913405080a
Fmnd <fmnd@yandex.ru>
2022-01-13 21:13:08 +0300

    Edit task #3

--- SQL_task-3_extra.sql
+++ SQL_task-3_extra.sql
@@ -1,19 +1,22 @@
-CREATE TABLE IF NOT EXISTS Employee (
+CREATE TABLE IF NOT EXISTS People (
 	id serial PRIMARY KEY,
 	Name varchar(40) not null
 );
 CREATE TABLE IF NOT EXISTS Department (
 	id serial PRIMARY KEY,
-	name varchar(40) not null
+	Name varchar(40) not null
 );
 CREATE TABLE IF NOT EXISTS Chief (
-	Employee_name varchar(40) not null references Employee(Name),
+	People_id integer not null references People(id),
 	Department_id integer not null references Department(id),
-    constraint pk PRIMARY KEY key(Employee_name, Department_id)
+    constraint pk PRIMARY KEY key(People_id, Department_id)
 );
-CREATE TABLE IF NOT EXISTS Employee_Department (
-	Employee_name varchar(40) not null references Employee(Name),
+/*
+    Решение
+*/
+CREATE TABLE IF NOT EXISTS Employee (
+	People_name varchar(40) not null references People(Name),
 	Department_id integer not null references Department(id),
-	Chief varchar(40) references Department(id),
-	constraint pk PRIMARY KEY key(Department_id, Employee_name)
+	Chief integer not null references Chief(People_id),
+	constraint pk PRIMARY KEY key(Department_id, People_name)
 );
